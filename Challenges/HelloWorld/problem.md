ee14993118b47a6ef4f650cb4440cf76cfd47667
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-26 12:58:54 -0300

    Add files via upload

---
+++ Challenges/HelloWorld/problem.md
@@ -0,0 +1,4 @@
+## Hello World
+
+Write a program that displays the message 'Hello, World!' on the screen.
+
---
+++ Challenges/HelloWorld/solution.js
@@ -0,0 +1,8 @@
+const readline = require("readline-sync");
+
+function helloWorld() {
+  console.log("Hello World !");
+}
+
+helloWorld();
+readline.question("Press Enter to Exit ... ");
