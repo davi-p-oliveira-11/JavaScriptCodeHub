72a717e833258d96bcb90c0e768f711a8600fc7f
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-26 13:06:19 -0300

    Create solution.js

---
+++ Challenges/HelloUser/solution.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+const readline = require('readline-sync');
+
+function helloUser () {
+  const userName = prompt('What is your name ? ' );
+
+  console.log(`Hello ${userName} nice to meet you !`);
+}
+
+helloUser();
+readline.question("Press Enter to Exit ... ");
