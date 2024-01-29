630a95d8552aa554da1f0fa1c3c0fe80b3b46047
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-29 00:12:09 -0300

    Create solution.js

---
+++ Challenges/DeltaCalculator/solution.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
+
+function deltaCalc() {
+  const valueA = parseInt(prompt("Enter the value of A "));
+  const valueB = parseInt(prompt("Enter the value of B "));
+  const valueC = parseInt(prompt("Enter the value of C "));
+
+  /* Bhaskara's formula:  delta = b² - 4ac */
+  const deltaValue = valueB ** 2 - 4 * (valueA * valueC);
+
+  console.log(`The value of delta is equal to: ${deltaValue}`);
+}
+
+deltaCalc();
+readline.question('Press Enter to Exit ...');
