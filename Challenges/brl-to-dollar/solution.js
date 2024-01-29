38f1468d541bb0a0fa27174521f0f1e6f47e2205
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-29 10:58:49 -0300

    fixed code format

--- Challenges/brl-to-dollar/solution.js
+++ Challenges/brl-to-dollar/solution.js
@@ -1,19 +1,15 @@
-const prompt = require('prompt-sync')();
-const readline = require('readline-sync');
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
 
-// Initial commit
-// update this to be dinamic like in the C program
+function brlToDollar() {
+  const availableBRL = parseFloat(prompt("Enter a value in BRL: "));
+  const usdValue = parseFloat(prompt("What is today's dollar exchange rate ?"));
+  const dollars = availableBRL / usdValue;
 
-function brlToDollar () {
+  // toFixed(2) to work better with financial values
 
-const availableBRL = parseFloat(prompt('Enter a value in BRL: '));
-const dollars = availableBRL / 3.45; 
-
-// toFixed(2) to work better with financial values
-
-console.log(`'You can buy a total of ${dollars.toFixed(2)}USD`);
-
+  console.log(`You can buy a total of ${dollars.toFixed(2)}USD`);
 }
 
 brlToDollar();
-readline.question('Press Enter to Exit ...')
+readline.question("Press Enter to Exit ...");
