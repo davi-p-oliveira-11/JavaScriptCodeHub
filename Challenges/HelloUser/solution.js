8edada27df821f637ca639a5fd86f8222c9f2c58
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-29 11:01:12 -0300

    Update solution.js

--- Challenges/HelloUser/solution.js
+++ Challenges/HelloUser/solution.js
@@ -1,13 +1,13 @@
-const prompt = require('prompt-sync')();
-const readline = require('readline-sync');
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
 
-// Initial Commit
-
-function helloUser () {
-  const userName = prompt('What is your name ? ' );
+function helloUser() {
+  const userName = prompt("What is your name ? ");
 
   console.log(`Hello ${userName} nice to meet you !`);
 }
 
 helloUser();
 readline.question("Press Enter to Exit ... ");
+
+// Initial commit and fixed formatting
