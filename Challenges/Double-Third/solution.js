aaa6ccaa9929cd536b7d22ad3122a0e342989321
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-30 12:12:32 -0300

    Create solution.js

---
+++ Challenges/Double-Third/solution.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+const readline = require('readline-sync');
+
+function doubleThird () {
+
+  const number = parseFloat(prompt("Enter a number "));
+
+  const doubleOf =  number * 2;
+  const thirdOf = number / 3;
+
+  console.log(`The double of ${number} is ${doubleOf.toFixed(2)}`);
+  console.log(`The third of ${number} is ${thirdOf.toFixed(2)}`);
+}
+
+doubleThird();
+readline.question();
