c9e34a8acd94f23197060daa757bbc06d840b480
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-05 11:28:54 -0300

    Create solution.js

---
+++ Challenges/EvenOrOdd/solution.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
+
+function isEvenOrOdd() {
+  const number = parseInt(prompt("Enter a number "));
+
+  if (number % 2 === 0) {
+    console.log("The number is even");
+    readline.question("Press Enter to Exit ... ");
+  } else {
+    console.log("The number is odd");
+    readline.question("Press Enter to Exit ... ");
+  }
+}
+
+isEvenOrOdd();
